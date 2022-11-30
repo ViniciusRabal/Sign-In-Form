058e2dc097de6b6564a8e778a0edebb1c1bcf73b
Vinicius Rabal <vinny.rodrigues85@gmail.com>
2022-11-29 22:08:49 -0300

    update js

--- assets/script.js
+++ assets/script.js
@@ -1,36 +1,70 @@
-const form = document.querySelector(".form");
-const username = document.querySelector(".username")
-const email = document.querySelector(".email")
-const password = document.querySelector(".password")
-const confirmPassword = document.querySelector(".confirm-password")
+const form = document.querySelector(".form")
+const button = document.querySelector(".button")
+const username = document.querySelector("#username")
+const email = document.querySelector("#email")
+const password = document.querySelector("#password")
+const confirmPassword = document.querySelector("#confirm-password")
 
 
 
-
-form.addEventListener("submit", (event) => {
+button.addEventListener("click", function(event) {
     event.preventDefault();
-    checkInputs()
+    checkInputs();
 } );
 
 function checkInputs () {
-    const usernameValue = username.value
-    const emailValue = email.value
-    const passwordValue = password.value
-    const confirmValue = confirmPassword.value
+    const usernameValue = username.value;
+    const emailValue = email.value;
+    const passwordValue = password.value;
+    const confirmValue = confirmPassword.value;
 
-    if (usernameValue === '') {
-        setErrorFor(username, 'Seu nome é obrigatório')
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if (usernameValue == "") {
+        setErrorFor(username, "Digite seu nome!");
     }else {
         setSuccessFor(username);
+    }
+
+    if (emailValue == "") {
+        setErrorFor(email, "Digite seu email!");
+    }else if (!filter.test(emailValue)){
+        setErrorFor(email, "Digite o email corretamente!")
+    }else {
+        setSuccessFor(email);
+    }
+
+    if (passwordValue == "") {
+        setErrorFor(password, "Digite uma senha!")
+    } else if (passwordValue.length <= 4) {
+        setErrorFor(password, "Digite ao menos 5 caracteres!")
+    }else{
+        setSuccessFor(password)
+    }
+    if (confirmValue == ""){
+        setErrorFor(confirmPassword, "Confirme sua senha!")
+    }else if (confirmValue != passwordValue) {
+        setErrorFor(confirmPassword, "As senhas não conferem!")
+    }else {
+        setSuccessFor(confirmPassword);
     }
 }
 
 function setErrorFor (input, message) {
-    message = document.querySelector("")
-    formControl = input.parentElement;
-    formControl.className = "form-controller error"
+    const formControl = input.parentElement;
+    const small = formControl.querySelector("small");
+    small.innerText = message;
 
+    formControl.className = "form-controller error"
 }
-function setSuccessFor () {
-    
+
+function setSuccessFor (input) {
+    const formControl = input.parentElement;
+    formControl.className = "form-controller success"
 }
+
+
+
+
+
+
